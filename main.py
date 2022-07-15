53bdb948036a3198b229397d631920ab4258b82d
Abhijitrk97 <95897040+Abhijitrk97@users.noreply.github.com>
2022-07-15 18:57:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -40,52 +40,79 @@ app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
 app = FastAPI(title='Abhi', description=app_desc)
 
 def read_imagefile(file) -> Image.Image:
-    image = Image.open(BytesIO(file))
+    try:
+        image = Image.open(BytesIO(file))
+    except Exception as e:
+        logging.error(f"Unable to read the image file, Please check if the image file ends with jpeg,jpg or png")
+        return 100
     return image
     
-#app.include_router(data.router)
-#app.include_router(users.router)
+
 app.include_router(authentication.router)
-#app.mount("/", StaticFiles(directory="ui", html=True), name="ui")
+
 
 models.Base.metadata.create_all(bind=engine)
 
 @app.post("/predict_with_augmented_data_trained_model")
 async def predict(file: UploadFile = File(...),get_current_user: schemas.ServiceAccount = Depends(get_current_user)):
     image = read_imagefile(await file.read())
-    np_image = np.array(image).astype('float32')/255
-    np_image = transform.resize(np_image, (300, 300, 1))
-    np_image = np.expand_dims(np_image, axis=0)
+    try:
+        np_image = np.array(image).astype('float32')/255
+        np_image = transform.resize(np_image, (300, 300, 1))
+        np_image = np.expand_dims(np_image, axis=0)
+    except Exception as e:
+        logging.error(f"Numpy unable to process the image")
+        return 100
     image = np_image
-    aughdf5=os.path.join(sys.path[0], "cnn_casting_inspection_model.hdf5")
-    model= load_model(aughdf5)
-    ypred= model.predict(image)
+    try:
+        aughdf5=os.path.join(sys.path[0], "cnn_casting_inspection_model.hdf5")
+        model= load_model(aughdf5)
+    except Exception as e:
+        logging.error(f"Unable to read the model")
+        return 100
+    try:
+        ypred= model.predict(image)
+    except Exception as e:
+        logging.error(f"Model not able to make prediction on given image")
+        return 100
     if ypred<0.5:
         result='ok'
     else:
         result='defect'
-    return {result}
+    return result
 
 @app.post("/predict_with_non_augmented_data_trained_model")
 async def predict(file: UploadFile = File(...),get_current_user: schemas.ServiceAccount = Depends(get_current_user)):
     image = read_imagefile(await file.read())
-    np_image = np.array(image).astype('float32')/255
-    np_image = transform.resize(np_image, (300, 300, 1))
-    np_image = np.expand_dims(np_image, axis=0)
+    try:
+        np_image = np.array(image).astype('float32')/255
+        np_image = transform.resize(np_image, (300, 300, 1))
+        np_image = np.expand_dims(np_image, axis=0)
+    except Exception as e:
+        logging.error(f"Numpy unable to process the image")
+        return 100
     image = np_image
-    nonaughdf5=os.path.join(sys.path[0], "cnn_casting_inspection_modelnonaug.hdf5")
-    model= load_model(nonaughdf5)
-    ypred= model.predict(image)
+    try:
+        nonaughdf5=os.path.join(sys.path[0], "cnn_casting_inspection_modelnonaug.hdf5")
+        model= load_model(nonaughdf5)
+    except Exception as e:
+        logging.error(f"Unable to read the model")
+        return 100
+    try:
+        ypred= model.predict(image)
+    except Exception as e:
+        logging.error(f"Model not able to make prediction on given image")
+        return 100
     print(ypred)
     if ypred>0.5:
         result='ok'
     else:
         result='defect'
-    return {result}
+    return result
 
 
 @app.get("/")
-async def index(get_current_user: schemas.ServiceAccount = Depends(get_current_user)):
+async def index():
     return RedirectResponse(url="/docs")
 
 
