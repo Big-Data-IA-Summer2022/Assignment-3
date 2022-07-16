f2d9f5b708ddf27634bd8919de29610cd4262ebc
Abhijit Kunjiraman <95897040+Abhijitrk97@users.noreply.github.com>
2022-07-16 11:19:43 -0400

    Update logfunc.py

--- logfunc.py
+++ logfunc.py
@@ -5,14 +5,14 @@ import sys
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =  os.path.join(sys.path[0], "key.json")
 
-def logfunc(endpoint:str, response_code: int):
+def logfunc(username:str, endpoint:str, response_code: int):
     client = bigquery.Client()
     max=client.query(f"""select max(logid)+1, string(current_timestamp()) as tstamp FROM `defect-detection-356414.for_logs.logs`""").result()
     for i in max:
         var= i[0]
         tstamp=i[1]
     print('log id',var,'has been populated')
-    rows_to_insert =[{"logid":var, "logtime":tstamp , "endpoint": endpoint, "response_code": response_code}]
+    rows_to_insert =[{"logid":var, "logtime":tstamp ,"username":username, "endpoint": endpoint, "response_code": response_code}]
     errors = client.insert_rows_json('defect-detection-356414.for_logs.logs', rows_to_insert)  # Make an API request.
     if errors == []:
         print("New rows have been added.")
